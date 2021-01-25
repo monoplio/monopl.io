e0290bd430c9d01fe2b1e76a73457b602b16df10
Justin Chang <changguangyu@yahoo.com>
2021-01-24 20:34:59 -0800

    Created join game mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_game, mutation: Mutations::Games::CreateGame, description: 'Creates a new noplio game'
+    field :join_game, mutation: Mutations::Games::JoinGame, description: 'Joins existing noplio game'
   end
 end
