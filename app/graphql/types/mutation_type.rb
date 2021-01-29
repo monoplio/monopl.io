42c4e89f89519c25e1b0b19bdb65ff72bc0af0c4
Justin Chang <changguangyu@yahoo.com>
2021-01-28 21:28:25 -0800

    Add ff mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,11 @@
 module Types
   class MutationType < Types::BaseObject
+    # Game Mutations
     field :create_game, mutation: Mutations::Games::CreateGame, description: 'Creates a new noplio game'
     field :join_game, mutation: Mutations::Games::JoinGame, description: 'Joins existing noplio game'
     field :start_game, mutation: Mutations::Games::StartGame, description: 'Starts noplio game'
+
+    # Turn Mutations
+    field :ff, mutation: Mutations::Turns::Ff, description: 'Player forfeits game'
   end
 end
