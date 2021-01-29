bda6afd3b12ad82fbf234a141c6bcdba5eec34b4
Justin Chang <changguangyu@yahoo.com>
2021-01-28 20:54:30 -0800

    rubocop

--- db/migrate/20210129044155_add_playing_attribute.rb
+++ db/migrate/20210129044155_add_playing_attribute.rb
@@ -1,4 +1,4 @@
-# Add playing boolean to player
+# Add playing boolean to player model
 class AddPlayingAttribute < ActiveRecord::Migration[6.1]
   def change
     add_column :players, :is_playing, :boolean
