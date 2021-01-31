3762e2285667c30cbd1fbfe13f3acf74887bb0d7
Justin Chang <changguangyu@yahoo.com>
2021-01-30 19:22:03 -0800

    Add buy house mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -6,8 +6,9 @@ module Types
     field :start_game, mutation: Mutations::Games::StartGame, description: 'Starts noplio game'
 
     # Turn Mutations
-    field :ff, mutation: Mutations::Turns::Ff, description: 'Player forfeits game'
     field :buy_house, mutation: Mutations::Turns::BuyHouse, description: 'Purchases house on property'
+    field :buy_property, mutation: Mutations::Turns::BuyProperty, description: 'Purchases property'
+    field :ff, mutation: Mutations::Turns::Ff, description: 'Player forfeits game'
     field :sell_house, mutation: Mutations::Turns::SellHouse, description: 'Sells house on property'
   end
 end
