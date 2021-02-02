1ea5ce2b521a1db15ea51098d03c47ab7414afa5
MathyouMB <matthewmacraebovell@bell.net>
2021-02-02 11:53:58 -0500

    rubocop

--- app/channels/graphql_channel.rb
+++ app/channels/graphql_channel.rb
@@ -1,14 +1,15 @@
+# Channel for GraphQL
 class GraphqlChannel < ApplicationCable::Channel
   def subscribed
     @subscription_ids = []
   end
 
   def execute(data)
-    query = data["query"]
-    variables = ensure_hash(data["variables"])
-    operation_name = data["operationName"]
+    query = data['query']
+    variables = ensure_hash(data['variables'])
+    operation_name = data['operationName']
     context = {
-      channel: self,
+      channel: self
     }
 
     result = BoardgameapiSchema.execute(
@@ -20,20 +21,18 @@ class GraphqlChannel < ApplicationCable::Channel
 
     payload = {
       result: result.subscription? ? { data: nil } : result.to_h,
-      more: result.subscription?,
+      more: result.subscription?
     }
 
-    if result.context[:subscription_id]
-      @subscription_ids << context[:subscription_id]
-    end
+    @subscription_ids << context[:subscription_id] if result.context[:subscription_id]
 
     transmit(payload)
   end
 
   def unsubscribed
-    @subscription_ids.each { |sid|
+    @subscription_ids.each do |sid|
       BoardgameapiSchema.subscriptions.delete_subscription(sid)
-    }
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
-  end
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
   mount ActionCable.server => '/cable'
 end
