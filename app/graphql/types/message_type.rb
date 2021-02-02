cc86c1c15d6228009fec584138ce9b45f5c7a320
MathyouMB <matthewmacraebovell@bell.net>
2021-02-02 15:11:13 -0500

    rubocop

--- app/graphql/types/message_type.rb
+++ app/graphql/types/message_type.rb
@@ -1,9 +1,8 @@
 module Types
-    # The MessageType is used for modelling the return of graphql subcriptions
-    class MessageType < Types::BaseObject
-      description 'A type used for modelling the return of graphql subcriptions.'
-      field :message, String, null: false
-      field :data, Types::GameType, null: false
-    end
+  # The MessageType is used for modelling the return of graphql subcriptions
+  class MessageType < Types::BaseObject
+    description 'A type used for modelling the return of graphql subcriptions.'
+    field :message, String, null: false
+    field :data, Types::GameType, null: false
   end
-  
+end
