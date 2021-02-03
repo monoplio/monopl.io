e24b5c1ba1db4b92e9a31ef887591bc359765961
Matthew MacRae-Bovell <matthewmacraebovell@bell.net>
2021-02-02 22:42:38 -0500

    GraphQL Subscription Scope (#62)

---
+++ app/graphql/subscriptions/base_subscription.rb
@@ -0,0 +1,8 @@
+module Subscriptions
+  class BaseSubscription < GraphQL::Schema::Subscription
+    # Hook up base classes
+    object_class Types::BaseObject
+    field_class Types::BaseField
+    argument_class Types::BaseArgument
+  end
+end
