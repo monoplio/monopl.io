2ebddb8f31f9bbcff1aaf42647e6771fe1b734ce
Matthew MacRae-Bovell <matthewmacraebovell@bell.net>
2021-02-04 18:21:45 -0500

    add game query (#68)

---
+++ app/graphql/queries/game.rb
@@ -0,0 +1,14 @@
+module Queries
+  # A Query that returns the information of a given game based on the given id
+  class Game < Queries::BaseQuery
+    description 'Get Game by id'
+    argument :id, ID, required: true
+    type Types::GameType, null: false
+
+    def resolve(id:)
+      ::Game.find(id)
+    rescue ActiveRecord::RecordNotFound
+      GraphQL::ExecutionError.new('ERROR: Image of given ID is nil')
+    end
+  end
+end
