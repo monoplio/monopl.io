eda8f83f473e9cb91cf09535ba095477383f4db5
MathyouMB <matthewmacraebovell@bell.net>
2021-02-04 21:31:06 -0500

    update graphql property set

--- app/graphql/types/property_set_type.rb
+++ app/graphql/types/property_set_type.rb
@@ -4,6 +4,7 @@ module Types
     description 'A type that represents a single Game entity.'
     field :id, ID, null: false
     field :name, String, null: false
+    field :color, String, null: false
     field :description, String, null: false
     field :properties, [Types::PropertyType], null: false
   end
