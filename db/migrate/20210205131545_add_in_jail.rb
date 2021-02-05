44f2b4c7b430a604100cbe87fc5fd6ac7db57450
Justin Chang <changguangyu@yahoo.com>
2021-02-05 05:36:31 -0800

    Fix missing :

--- db/migrate/20210205131545_add_in_jail.rb
+++ db/migrate/20210205131545_add_in_jail.rb
@@ -1,6 +1,6 @@
 # Add in jail migration
 class AddInJail < ActiveRecord::Migration[6.1]
   def change
-    add_column :players, :in_jail, boolean
+    add_column :players, :in_jail, :boolean
   end
 end
