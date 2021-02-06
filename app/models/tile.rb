5a4004b9b73636b4753be9542d7d5f6893ccf190
MathyouMB <matthewmacraebovell@bell.net>
2021-02-05 23:42:42 -0500

    added current players to tile

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,4 +1,8 @@
 class Tile < ApplicationRecord
   belongs_to :board_tile, polymorphic: true
   belongs_to :game
+
+  def current_players
+    Player.where(x: self.x, game_id: self.game_id)
+  end
 end
