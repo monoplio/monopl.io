ec9c561da31354b2a0d7fce59318124e65e1c03a
MathyouMB <matthewmacraebovell@bell.net>
2021-02-15 17:41:50 -0500

    wrote landing cost logic

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,12 +3,19 @@ class Property < ApplicationRecord
   belongs_to :player, optional: true
   belongs_to :property_set
   has_one :tile, as: :board_tile, dependent: :destroy
-  has_many :property_rents
+  has_many :property_rents, dependent: :destroy
 
   def landing_cost
     # Needs logic for rail roads which work differently due to the stage being dependant on ownership
-    unless property_rents.empty?
-      property_rents.find_by(stage: stage).rent
+    if property_rents.empty?
+      0
+    else
+      rent = property_rents.find_by(stage: stage).rent
+      if stage.zero? # and If you own the full set (this needs to be added)
+        rent * 2
+      else
+        rent
+      end
     end
   end
 end
