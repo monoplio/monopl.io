278c328150323d19b05099a23a5eb19adf47a00e
MathyouMB <matthewmacraebovell@bell.net>
2021-02-21 23:28:58 -0500

    update syntax

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,6 +1,6 @@
 # Model for trades table
 class Trade < ApplicationRecord
-  has_one :game, class_name: 'Game', foreign_key: 'current_trade_id', required: false, inverse_of: trade, dependent: :destroy
+  has_one :game, class_name: 'Game', foreign_key: 'current_trade_id', required: false, inverse_of: :trade, dependent: :destroy
   belongs_to :owner, class_name: 'Player', optional: true
   belongs_to :customer, class_name: 'Player', optional: true
 
