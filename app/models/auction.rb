69634ef3de969245efc8125b16a6642fb4664ebb
Justin Chang <changguangyu@yahoo.com>
2021-02-27 07:16:26 -0800

    fix rubocop

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,5 +1,5 @@
 class Auction < ApplicationRecord
-  has_one :game
+  has_one :game, dependent: :destroy
   belongs_to :property, class_name: 'Property', optional: false
   belongs_to :bid, class_name: 'Bid', optional: true
 end
--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,4 @@
 class Bid < ApplicationRecord
-    has_one :auction
-    belongs_to :player, class_name: 'Player', optional: false
+  has_one :auction, dependent: :destroy
+  belongs_to :player, class_name: 'Player', optional: false
 end
